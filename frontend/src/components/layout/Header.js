cd9ee6e8023ef7e3dffb0ae4e72cf403778d767f
Lorik <lorikrraci00@gmail.com>
2025-01-19 19:49:34 +0100

    123

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import Search from './Search';
+import {Route, Link} from 'react-router-dom'
+
+export const Header = () => {
+    return (
+        <Fragment>
+            <nav className="navbar row">
+                <div className="col-12 col-md-3">
+                    <div className="navbar-brand">
+                        <img src="./images/shopit.logo.png" alt="Logo" />
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 mt-2 mt-md-0">
+                    <Search />
+                </div>
+
+                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                    <Link to="/login" className="btn ml-4" id="login_btn">Login</Link>
+
+                    <span id="cart" className="ml-3">Cart</span>
+                    <span className="ml-1" id="cart_count">2</span>
+                </div>
+            </nav>
+        </Fragment>
+    );
+}
+
+export default Header;
